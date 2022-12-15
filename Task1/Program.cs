635cdc7b9038d8f84f78b094408031b5b8649c05
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-15 18:49:39 +0700

    HWSem5-1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+Console.WriteLine("Enter size array:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+FillArrayRandomNumbers(arr);
+PrintArray(arr);
+int count = 0;
+
+for (int z = 0; z < arr.Length; z++)
+if (arr[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"=> {count} even numbers");
+
+void FillArrayRandomNumbers(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
