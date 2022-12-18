0b92ffc7d910437de372a46e20162d896e4e1079
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-18 23:22:26 +0700

    HWSem5-3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76*/
+
+Console.WriteLine("Enter size array: ");
+int size = int.Parse(Console.ReadLine());
+float[] arr = new float [size];
+ArrayRandomNumbers(arr);
+PrintArray(arr);
+float min = Int32.MaxValue;
+float max = Int32.MinValue;
+
+void ArrayRandomNumbers(float[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0,100);
+    }
+}
+
+void PrintArray(float[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + ",");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+}
+
+Console.WriteLine($"MAX => {max}, MIN => {min}. Difference between MAX and MIN values = {max - min}");
+
+//Не могу разобраться, как убрать запятую в конце массива..
